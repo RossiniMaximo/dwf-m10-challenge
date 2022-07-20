83a1f3cb30b2d89089eaa463bebdf65af0f46830
Maximo <maximorossini2016@gmail.com>
2022-07-20 15:56:23 -0300

    deleted error boundaries

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { ErrorBoundary } from "components/errorBoundary";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
+      <Component {...pageProps} />
     </RecoilRoot>
   );
 }
